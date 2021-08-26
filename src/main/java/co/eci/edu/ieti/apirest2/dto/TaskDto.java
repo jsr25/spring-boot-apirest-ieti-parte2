42f40076e2341760dabe65d3e5674e0ed7189ace
jsr25 <ramosjuans@live.com.ar>
2021-08-25 23:44:08 -0500

    taskdto created

---
+++ src/main/java/co/eci/edu/ieti/apirest2/dto/TaskDto.java
@@ -0,0 +1,66 @@
+package co.eci.edu.ieti.apirest2.dto;
+
+import java.util.Date;
+
+public class TaskDto {
+
+    private String name;
+
+    private String description;
+
+    private String Status;
+
+    private String assignedTo;
+
+    private Date dueDate;
+
+    private Date created;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
