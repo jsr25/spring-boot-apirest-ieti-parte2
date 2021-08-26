68b86d9b67ce2f64b598c10b994aebfb33de4eb6
jsr25 <ramosjuans@live.com.ar>
2021-08-25 23:19:14 -0500

    first commit

---
+++ src/main/java/co/eci/edu/ieti/apirest2/Apirest2Application.java
@@ -0,0 +1,13 @@
+package co.eci.edu.ieti.apirest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirest2Application.class, args);
+	}
+
+}
